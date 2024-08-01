1ac8f9d0b30e3d46ff900ac419a3e67113540543
fadhilijane <janefadhili@gmail.com>
2024-08-01 14:22:13 +0000

    7

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''multiples args'''
+from typing import Callable
+
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''creates the multiplier function'''
+    return lambda x: x * multiplier
