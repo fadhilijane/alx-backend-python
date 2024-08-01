3d3cfd5a5660f96ebe89dee435d6b6f62b66788a
fadhilijane <janefadhili@gmail.com>
2024-08-01 13:04:08 +0000

    3

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''module that sum numbers in a list'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''adding the numbers in the list'''
+    return sum(input_list)
